9b6669aa05d0bc7a52cbab16c7021d7f2347dc89
mykhailo.dvolitka <mykhailo.dvolitka@gmail.com>
2016-01-12 14:36:18 +0200

    Initial commit

---
+++ src/main/java/Patterns/factory_method/factory.java
@@ -0,0 +1,43 @@
+package Patterns.factory_method;
+
+interface Dog{
+    public void speak();
+}
+class Poodle implements Dog{
+    public void speak(){
+        System.out.println("The Poodle say \"arf\"");
+    }
+}
+class Rottweiler implements Dog{
+    public void speak(){
+        System.out.println("The Rottweiler says \"WOOF!\"");
+    }
+}
+class Husky implements Dog{
+    public void speak(){
+        System.out.println("The Husky says\"Dude, what's up?\"");
+    }
+}
+class DogFactory{
+    public static Dog getDog(String criteria){
+        if (criteria.equals("small")) {
+            return new Poodle();
+        } else if(criteria.equals("big")){
+            return new Rottweiler();
+        } else if (criteria.equals("woking")){
+            return new Husky();
+        }
+        return null;
+    }
+}
+
+public class factory {
+    public static void main(String[] args) {
+Dog dog=DogFactory.getDog("small");
+        dog.speak();
+        dog = DogFactory.getDog("big");
+        dog.speak();
+        dog =DogFactory.getDog("working");
+        dog.speak();
+    }
+}
