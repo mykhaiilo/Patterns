b2cbc71453974492c371d3d6c1bb1aa39daff4d5
mykhailo.dvolitka <mykhailo.dvolitka@gmail.com>
2016-01-12 17:04:38 +0200

    .

---
+++ src/main/java/Patterns/Singleton/Singleton.java
@@ -0,0 +1,17 @@
+package Patterns.Singleton;
+
+/**
+ * Created by admin on 12.01.2016.
+ */
+public final class Singleton {
+    private static Singleton _instance = null;
+
+    private Singleton() {}
+
+    public static synchronized Singleton getInstance() {
+        if (_instance == null)
+            _instance = new Singleton();
+        return _instance;
+    }
+}
+}
